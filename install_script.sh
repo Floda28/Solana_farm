9fb45deaea4bdc990722b8e4eeb1eca35bdc3e2a
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:03:07 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -6,5 +6,3 @@ apt-get install python3.12 -y || { echo "Не удалось установит�
 apt install python3-pip -y || { echo "Не удалось установить pip"; exit 1; }
 pip3 install -r requirements.txt || { echo "Не удалось установить зависимости"; exit 1; }
 screen -S Sonic_auto_transaction || { echo "Не удалось создать screen сессию"; exit 1; }
-
-# Добавьте пустую строку в конце файла
