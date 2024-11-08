c117835164b1d25241438e327a1a8d76be629aa3
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:25:10 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/Floda28/Solana_farm.git && \
-
 apt-get update && \
-
 apt-get install python3.12 -y && \
-
 apt-get install python3-pip -y && \
-
 cd Solana_farm && \
-
 pip3 install -r requirements.txt
