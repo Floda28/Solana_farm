7609bc9948148a6830a7df5cd0c5652425156cf9
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:18:04 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 git clone https://github.com/Floda28/Solana_farm.git && \
+
 apt-get update && \
+
 apt-get install python3.12 -y && \
+
 apt-get install python3-pip -y && \
+
 cd Solana_farm && \
+
 pip3 install -r requirements.txt && \
+
 screen -dmS Sonic_auto_transaction
