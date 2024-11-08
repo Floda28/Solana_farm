f4b7965b5e78e83b0de9a8abad0dd873efce5eb1
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:07:00 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -p Sonic && cd Sonic || { echo "Не удалось создать или зайти в папку Sonic"; exit 1; }
-git clone https://github.com/Floda28/Solana_farm.git . || { echo "Не удалось клонировать репозиторий"; exit 1; }
+mkdir -p Sonic && cd Sonic
+git clone https://github.com/Floda28/Solana_farm.git
 apt update
-apt-get install python3.12 -y || { echo "Не удалось установить Python 3.12"; exit 1; }
-apt install python3-pip -y || { echo "Не удалось установить pip"; exit 1; }
-pip3 install -r requirements.txt || { echo "Не удалось установить зависимости"; exit 1; }
-screen -S Sonic_auto_transaction || { echo "Не удалось создать screen сессию"; exit 1; }
+apt-get install python3.12 -y || 
+apt install python3-pip -y 
+pip3 install -r requirements.txt
+screen -S Sonic_auto_transaction 
 
 # Пустая строка в конце файла
