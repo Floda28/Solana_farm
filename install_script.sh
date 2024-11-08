9e4def558555239d43671543b9514f2e3f6e4c5a
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:34:56 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/Floda28/Solana_farm.git && \
 apt-get update && \
+apt-get install software-properties-common -y && \
 add-apt-repository ppa:deadsnakes/ppa -y && \
 apt-get update && \
 apt-get install python3.12 -y && \
