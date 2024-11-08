e0b0a59132e87e7446bf51c454ca3bb5e8c8d2be
Floda28 <144562353+Floda28@users.noreply.github.com>
2024-11-09 01:10:21 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-mkdir -p Sonic  
-cd Sonic
 git clone https://github.com/Floda28/Solana_farm.git
 apt update
 apt-get install python3.12 -y
 apt install python3-pip -y 
+cd Solana_farm 
 pip3 install -r requirements.txt
-screen -S Sonic_auto_transaction 
+screen -S Sonic_auto_transaction  
